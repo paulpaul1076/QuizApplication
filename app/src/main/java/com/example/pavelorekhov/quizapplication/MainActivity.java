3dc7f2d103bc77031f811e38cc860fa77eb99b3b
pavel.orekhov <pavel.orekhov@accenture.com>
2018-06-25 13:46:28 +0300

    Added landscape layout.

--- app/src/main/java/com/example/pavelorekhov/quizapplication/MainActivity.java
+++ app/src/main/java/com/example/pavelorekhov/quizapplication/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.pavelorekhov.quizapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String TAG = "QuizActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button nextButton = findViewById(R.id.next_button);
+        final Button prevButton = findViewById(R.id.prev_button);
+        final Button trueButton = findViewById(R.id.true_button);
+        final Button falseButton = findViewById(R.id.false_button);
+        final TextView textView = findViewById(R.id.text_view);
+
+        textView.setText(QuestionModel.questions[QuestionModel.getCurrentQuestionPos()].getText());
+
+        nextButton.setOnClickListener(view -> {
+            textView.setText(QuestionModel.questions[QuestionModel.moveToNextQuestionPos()].getText());
+        });
+
+        prevButton.setOnClickListener(view -> {
+            textView.setText(QuestionModel.questions[QuestionModel.moveToPrevQuestionPos()].getText());
+        });
+
+        trueButton.setOnClickListener(view -> {
+            QuestionModel.Question curQ = QuestionModel.questions[QuestionModel.getCurrentQuestionPos()];
+            if (curQ.isAnswer()) {
+                Toast.makeText(this, "Correcto!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Incorrecto!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        falseButton.setOnClickListener(view -> {
+            QuestionModel.Question curQ = QuestionModel.questions[QuestionModel.getCurrentQuestionPos()];
+            if (curQ.isAnswer()) {
+                Toast.makeText(this, "Incorrecto!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Correcto!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Log.d(TAG, "Everything is done");
     }
 }
---
+++ app/src/main/java/com/example/pavelorekhov/quizapplication/QuestionModel.java
@@ -0,0 +1,53 @@
+package com.example.pavelorekhov.quizapplication;
+
+import android.support.annotation.StringRes;
+
+public class QuestionModel {
+    public static Question[] questions = {
+            new Question(R.string.q1, true),
+            new Question(R.string.q2, false),
+            new Question(R.string.q3, true),
+            new Question(R.string.q4, true),
+            new Question(R.string.q5, false),
+            new Question(R.string.q6, true)
+    };
+    private static int currentQuestionPos = 0;
+
+    public static int getCurrentQuestionPos() {
+        return currentQuestionPos;
+    }
+
+    public static int moveToNextQuestionPos() {
+        currentQuestionPos = (currentQuestionPos + 1) % questions.length;
+        return currentQuestionPos;
+    }
+
+    public static int moveToPrevQuestionPos() {
+        if (currentQuestionPos == 0) {
+            currentQuestionPos = questions.length - 1;
+        } else {
+            currentQuestionPos = currentQuestionPos - 1;
+        }
+        return currentQuestionPos;
+    }
+
+    public static class Question {
+        private @StringRes
+        int text;
+        private boolean answer;
+
+        public Question(@StringRes int text, boolean answer) {
+            this.text = text;
+            this.answer = answer;
+        }
+
+        public @StringRes
+        int getText() {
+            return text;
+        }
+
+        public boolean isAnswer() {
+            return answer;
+        }
+    }
+}
